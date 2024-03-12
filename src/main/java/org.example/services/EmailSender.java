269eb8d73ae629ed2855a4b5aa4de89387d2dfde
Saule Anafinova <saule.anafinova@gmail.com>
2024-03-12 14:04:50 +0100

    First commit

--- src/main/java/org.example/services/EmailSender.java
+++ src/main/java/org.example/services/EmailSender.java
@@ -1,9 +1,13 @@
-package services;
+package org.example.services;
 
 import java.io.ByteArrayOutputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
 import java.util.Properties;
 
 import javax.mail.Authenticator;
@@ -22,6 +26,9 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
+
+import static org.apache.pdfbox.pdmodel.font.Standard14Fonts.FontName.HELVETICA_BOLD;
 
 public class EmailSender {
     private Properties sessionProps;
@@ -34,16 +41,15 @@ public class EmailSender {
     }
 
     public void setProps() {
-        try (FileInputStream sessionFile = new FileInputStream("src/files/session.properties");
-             FileInputStream librarySenderFile = new FileInputStream("src/files/librarySender.properties")) {
-            sessionProps.load(sessionFile);
-            librarySenderProps.load(librarySenderFile);
+        try {
+            sessionProps.load(getClass().getClassLoader().getResourceAsStream("files/session.properties"));
+            librarySenderProps.load(getClass().getClassLoader().getResourceAsStream("files/librarySender.properties"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void sendPdfReport(String header, String text, UserBase to) {
+    public void sendPdfReport(String header, String text, List<Person> to) {
         ByteArrayOutputStream pdfOutputStream = generatePdfReport(header, text);
 
         Session session = Session.getInstance(sessionProps, new Authenticator() {
@@ -69,15 +75,12 @@ public class EmailSender {
             pdfAttachment.setContent(pdfOutputStream.toByteArray(), "application/pdf");
             multipart.addBodyPart(pdfAttachment);
 
+            for (Person person : to) {
+                message.addRecipient(Message.RecipientType.TO, new InternetAddress(person.getEmail()));
+            }
+
             message.setContent(multipart);
 
-            StringBuilder emails = new StringBuilder();
-            for (User user : to.getUserList()) {
-                emails.append(user.getEmail());
-                emails.append(", ");
-            }
-            String emailsToSend = emails.toString();
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(emailsToSend));
             Transport.send(message);
             System.out.println("PDF report email sent!");
 
@@ -101,20 +104,49 @@ public class EmailSender {
 
             try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
                 contentStream.beginText();
-                contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
+                contentStream.setFont(new PDType1Font(Standard14Fonts.FontName.HELVETICA_BOLD), 12);
                 contentStream.newLineAtOffset(100, 700);
                 contentStream.showText(header);
                 contentStream.newLine();
-                contentStream.setFont(PDType1Font.HELVETICA, 10);
+                contentStream.setFont(new PDType1Font(Standard14Fonts.FontName.HELVETICA), 12);
                 contentStream.showText(text);
                 contentStream.endText();
+
+                // Fetch data from the database
+                String jdbcURL = "jdbc:your_database_url";
+                String username = "your_username";
+                String password = "your_password";
+                try (Connection connection = DriverManager.getConnection(jdbcURL, username, password)) {
+                    String query = "SELECT name, email FROM person";
+                    try (Statement statement = connection.createStatement();
+                         ResultSet resultSet = statement.executeQuery(query)) {
+                        // Iterate over the results and add them to the PDF
+                        int yCoordinate = 680; // Start y-coordinate for the text
+                        while (resultSet.next()) {
+                            String personName = resultSet.getString("name");
+                            String email = resultSet.getString("email");
+
+                            // Write person details to the PDF
+                            contentStream.beginText();
+                            contentStream.setFont(new PDType1Font(Standard14Fonts.FontName.HELVETICA), 12);
+                            contentStream.newLineAtOffset(100, yCoordinate);
+                            contentStream.showText("Person: " + personName);
+                            contentStream.newLine();
+                            contentStream.showText("Email: " + email);
+                            contentStream.newLine();
+                            contentStream.endText();
+
+                            yCoordinate -= 40; // Move to the next line
+                        }
+                    }
+                }
             }
 
             document.save(outputStream);
-        } catch (IOException e) {
+        } catch (IOException | SQLException e) {
             e.printStackTrace();
         }
         return outputStream;
     }
-}
 
+}
