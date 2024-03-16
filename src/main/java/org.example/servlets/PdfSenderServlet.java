7ae463e8d442d3077cabba95a26972c7dec62387
Алёна Чжен <alenakuzmina629@gmail.com>
2024-03-16 14:43:11 +0300

    changed time of sending

--- src/main/java/org.example/servlets/PdfSenderServlet.java
+++ src/main/java/org.example/servlets/PdfSenderServlet.java
@@ -13,6 +13,6 @@ public class PdfSenderServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
         super.init();
-        ScheduledMain.scheduleExecution(14, 40);
+        ScheduledMain.scheduleExecution(14, 50);
     }
 }
