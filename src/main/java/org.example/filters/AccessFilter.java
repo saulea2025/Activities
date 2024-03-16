94c0773b4778fde1a52b1c4d24ac5afa2d786a6f
Adilbek Maratov <bigyellowforjava@gmail.com>
2024-03-16 22:19:58 +0100

    some changes

--- src/main/java/org.example/filters/AccessFilter.java
+++ src/main/java/org.example/filters/AccessFilter.java
@@ -15,7 +15,7 @@ import static java.util.Objects.nonNull;
 @WebFilter("/*")
 public class AccessFilter implements Filter {
     private static final List<String> AUTHORIZED_ALLOWED_PAGES = Arrays.asList("/login", "/logout", "/users", "/activities");
-    private static final List<String> UNAUTHORIZED_ALLOWED_PAGES = Collections.singletonList("/login");
+    private static final List<String> UNAUTHORIZED_ALLOWED_PAGES = Arrays.asList("/login", "/logout", "/users", "/activities");
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
