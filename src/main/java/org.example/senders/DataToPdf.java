d37ae011867f0ae913d3afe12649246c933971ec
alena chzhen <alenachzhen1999@gmail.com>
2024-03-17 14:28:39 +0300

    deleted pdf sending from login servlet

--- src/main/java/org.example/senders/DataToPdf.java
+++ src/main/java/org.example/senders/DataToPdf.java
@@ -25,7 +25,7 @@ public class DataToPdf {
             document.addPage(page);
             PDPageContentStream contentStream = new PDPageContentStream(document, page);
 
-            Standard14Fonts.FontName fontName = Standard14Fonts.getMappedFontName("HELVETICA_BOLD");
+            Standard14Fonts.FontName fontName = Standard14Fonts.FontName.HELVETICA;
             PDType1Font pdFont = new PDType1Font(fontName);
             contentStream.setFont(pdFont, 10);
 
